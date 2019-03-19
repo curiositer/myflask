68e54248591087805934bb3e62698962c75a0bea
zy--John <zydesj@qq.com>
2019-03-19 12:29:33 +0800

    文本框采取统一子模版_field.html 处理用户id相同的错误 添加竞赛（功能） 实现不同用户拥有不同功能权限 添加其他数据库表models 中文界面 尝试邮件（未成功）

--- app/forms.py
+++ app/forms.py
@@ -1,30 +1,31 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SelectField, BooleanField, SubmitField
-from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, Length
 from app.models import User
 
 
 class LoginForm(FlaskForm):
-    id = StringField('ID', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('Remember Me')
-    submit = SubmitField('Sign In')
+    id = StringField('用户名', validators=[DataRequired('请输入用户名')])
+    password = PasswordField('密码', validators=[DataRequired('请输入密码')])
+    remember_me = BooleanField('记住我')
+    submit = SubmitField('登陆')
 
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    id = StringField('ID', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    username = StringField('姓名', validators=[DataRequired()])
+    id = StringField('用户名', validators=[DataRequired()])
+    email = StringField('邮箱', validators=[DataRequired(), Email()])
+    password = PasswordField('密码', validators=[DataRequired()])
     password2 = PasswordField(
-        'Repeat Password', validators=[DataRequired(), EqualTo('password')])
-    type = SelectField('Type', choices=[(0, '管理员'), (1, '学生'), (2, '教师')], coerce=int)
-    submit = SubmitField('Register')
+        '确认密码', validators=[DataRequired(), EqualTo('password')])
+    type = SelectField('用户类型', choices=[(0, '管理员'), (1, '学生'), (2, '教师')], coerce=int)
+    submit = SubmitField('注册')
 
-    def validate_username(self, id):
+    def validate_id(self, id):
         id_num = User.query.filter_by(id=id.data).first()
+        print(id.data)
         if id_num is not None:
-            raise ValidationError('Please use a different id.')
+            raise ValidationError('该ID已被使用！')
 
 
 class EditProfileForm(FlaskForm):
@@ -32,3 +33,21 @@ class EditProfileForm(FlaskForm):
     email = StringField('电子邮箱', validators=[DataRequired()])
     # about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
     submit = SubmitField('提交修改')
+
+    def __init__(self, original_username, *args, **kwargs):
+        super(EditProfileForm, self).__init__(*args, **kwargs)
+        self.original_username = original_username
+
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            user = User.query.filter_by(username=self.username.data).first()
+            if user is not None:
+                raise ValidationError('该用户名已存在，请更换一个！')
+
+
+class AddContestForm(FlaskForm):
+    name = StringField('竞赛名', validators=[DataRequired()])
+    type = StringField('竞赛类型', validators=[DataRequired()])
+    details = StringField('竞赛描述', validators=[Length(min=0, max=150)])
+    level = StringField('竞赛等级', validators=[DataRequired()])
+    submit = SubmitField('添加竞赛')
