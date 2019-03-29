b196fe1fb9612c66f184e3d178ed1954ceff4ff2
zy--John <zydesj@qq.com>
2019-03-29 18:34:10 +0800

    邮件功能所有功能都实现后再尝试 完成UI界面初步美化

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_mail import Mail
+from flask_bootstrap import Bootstrap
 from config import Config
 
 app = Flask(__name__)
@@ -12,6 +14,8 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
+mail = Mail(app)
+bootstrap = Bootstrap(app)
 login.login_view = 'login'
 
 # if not app.debug:
