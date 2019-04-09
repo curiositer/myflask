35e474992a481ea3a4b0bd56c9833c414bb6b8f3
zy--John <zydesj@qq.com>
2019-04-09 14:17:15 +0800

    注册能正确提交 继承关系冗余admin表删除 form表单中将注册与添加用户分离 审核改为跳转到request_details页面实现

--- app/forms.py
+++ app/forms.py
@@ -12,10 +12,11 @@ class LoginForm(FlaskForm):
     submit = SubmitField('登陆')
 
 
-class RegistrationForm(FlaskForm):
-    username = StringField('姓名', validators=[DataRequired()])
+class AddUserForm(FlaskForm):
+    type = SelectField('用户类型', choices=[('admin', '管理员'), ('student', '学生'), ('teacher', '教师')], coerce=str)
+    username = StringField('姓名', validators=[DataRequired('请输入姓名')])
     user_id = StringField('学号', validators=[DataRequired('请输入学号')])
-    email = StringField('邮箱', validators=[DataRequired(), Email()])
+    email = StringField('邮箱', validators=[DataRequired('请输入邮箱'), Email('请输入正确邮箱格式')])
     tel_num = StringField('联系电话', validators=[DataRequired('请输入联系电话')])
     stu_class = StringField('班级(学生、教师填写)')
     tea_type = SelectField('教师类型（仅教师填写）',
@@ -23,13 +24,10 @@ class RegistrationForm(FlaskForm):
     password = PasswordField('密码', validators=[DataRequired('请输入密码')])
     password2 = PasswordField(
         '确认密码', validators=[DataRequired(), EqualTo('password')])
-    type = SelectField('用户类型', choices=[('admin', '管理员'), ('student', '学生'), ('teacher', '教师')], coerce=str)
     submit = SubmitField('注册')
 
-    def validate_id(self, user_id):
+    def validate_user_id(self, user_id):
         id_num = User.query.filter_by(user_id=user_id.data).first()
-        # print(id.data)
-        # print(id_num)
         if id_num is not None:
             raise ValidationError('该ID已被使用！')
 
@@ -42,6 +40,28 @@ class RegistrationForm(FlaskForm):
         if self.type.data == 'teacher':
             if tea_type.data == -1:
                 raise ValidationError('请选择教师类型！')
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('姓名', validators=[DataRequired('请输入姓名')])
+    user_id = StringField('学号', validators=[DataRequired('请输入学号')])
+    email = StringField('邮箱', validators=[DataRequired('请输入邮箱'), Email('请输入正确邮箱格式')])
+    tel_num = StringField('联系电话', validators=[DataRequired('请输入联系电话')])
+    stu_class = StringField('班级')
+    password = PasswordField('密码', validators=[DataRequired('请输入密码')])
+    password2 = PasswordField(
+        '确认密码', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('注册')
+
+    def validate_user_id(self, user_id):
+        id_num = User.query.filter_by(user_id=user_id.data).first()
+        if id_num is not None:
+            raise ValidationError('该ID已被使用！')
+
+    def validate_email(self, email):
+        email_list = User.query.filter_by(email=email.data).first()
+        if email_list is not None:
+            raise ValidationError('该邮箱已被使用！')
 
 
 class EditProfileForm(FlaskForm):
