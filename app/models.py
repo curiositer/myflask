b551905dd25c0b654879cd1145c297af2c136a41
zy--John <zydesj@qq.com>
2019-05-27 20:15:13 +0800

    提交版本0.5.5-去掉用户表里的index

--- app/models.py
+++ app/models.py
@@ -17,7 +17,7 @@ from datetime import datetime
 class User(UserMixin, db.Model):
     __tablename__ = 'user'
     user_id = db.Column(db.Integer, primary_key=True, unique=True)
-    username = db.Column(db.String(64), index=True, nullable=True)
+    username = db.Column(db.String(64), nullable=True)
     email = db.Column(db.String(120), unique=True, nullable=True)
     tel_num = db.Column(db.String(20))
     password_hash = db.Column(db.String(128), nullable=True)
